e16e4f9f0f470025b25150fa94ce51db650ec8a9
Chrish Dunne <hey@chrish.design>
2020-06-20 16:45:29 +0100

    [+] Added max height

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -4,14 +4,12 @@ import Layout from '../components/foundations/Layout';
 import Banner from '../components/blocks/Banner';
 import AboutUs from '../components/blocks/AboutUs';
 import ProjectsIndex from '../components/blocks/ProjectsIndex';
-import LogoMatrix from '../components/blocks/LogoMatrix';
 
 const IndexPage = () => (
   <Layout>
     <Banner />
     <AboutUs />
     <ProjectsIndex />
-    <LogoMatrix />
   </Layout>
 );
 
