572d7719525afc7d786e945e8ba45a9e501bbcb0
Chrish Dunne <hey@chrish.design>
2020-06-20 17:59:43 +0100

    [~] Needs more work

--- web/src/components/blocks/LogoMatrix/LogoMatrix.js
+++ web/src/components/blocks/LogoMatrix/LogoMatrix.js
@@ -1,7 +1,5 @@
 import React, { useEffect } from 'react';
-import $ from 'jquery';
-import 'bootstrap';
-import 'owl.carousel';
+import Flickity from 'flickity';
 
 import PlaceholderImg from '../../../../static/images/resource/client-logo-1.jpg';
 import PlaceholderImgAlt from '../../../../static/images/resource/client-logo-2.jpg';
@@ -12,45 +10,32 @@ import PlaceholderImgAlt from '../../../../static/images/resource/client-logo-2.
 
 const LogoMatrix = () => {
   useEffect(() => {
-    $('.theme_carousel').each(function (index) {
-      const $owlAttr = {},
-        $extraAttr = $(this).data('options');
-      $.extend($owlAttr, $extraAttr);
-      $(this).owlCarousel($owlAttr);
+    new Flickity('.theme_carousel', {
+      accessibility: true,
+      groupCells: true,
+      wrapAround: true,
+      draggable: '>1',
     });
   });
 
   return (
     <section className="clients-logo-section">
       <div className="auto-container">
-        <div
-          className="theme_carousel owl-theme owl-carousel"
-          data-options='{"loop": true, "margin": 40, "autoheight":true, "lazyload":true, "nav": true, "dots": true, "autoplay": true, "autoplayTimeout": 6000, "smartSpeed": 300, "responsive":{ "0" :{ "items": "1" }, "600" :{ "items" : "2" }, "768" :{ "items" : "3" } , "992":{ "items" : "4" }, "1200":{ "items" : "5" }}}'
-        >
-          <div className="slide-item">
-            <div className="image">
-              <img src={PlaceholderImg} alt="" />
-            </div>
+        <div className="theme_carousel">
+          <div className="image">
+            <img src={PlaceholderImg} alt="" />
           </div>
-          <div className="slide-item">
-            <div className="image">
-              <img src={PlaceholderImgAlt} alt="" />
-            </div>
+          <div className="image">
+            <img src={PlaceholderImgAlt} alt="" />
           </div>
-          <div className="slide-item">
-            <div className="image">
-              <img src={PlaceholderImg} alt="" />
-            </div>
+          <div className="image">
+            <img src={PlaceholderImg} alt="" />
           </div>
-          <div className="slide-item">
-            <div className="image">
-              <img src={PlaceholderImgAlt} alt="" />
-            </div>
+          <div className="image">
+            <img src={PlaceholderImgAlt} alt="" />
           </div>
-          <div className="slide-item">
-            <div className="image">
-              <img src={PlaceholderImg} alt="" />
-            </div>
+          <div className="image">
+            <img src={PlaceholderImg} alt="" />
           </div>
         </div>
       </div>
