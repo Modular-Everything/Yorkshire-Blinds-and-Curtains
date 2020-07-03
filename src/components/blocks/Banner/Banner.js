50e6ea9e09c7c25c1e454aa6d8c4919608147ad4
Chrish Dunne <hey@chrish.design>
2020-07-03 15:55:55 +0100

    [+] Added CMS editing option

--- src/components/blocks/Banner/Banner.js
+++ src/components/blocks/Banner/Banner.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import Swiper from 'swiper';
+import SbEditable from 'storyblok-react';
 
 // =====================
 // Banner
@@ -8,6 +9,8 @@ import Swiper from 'swiper';
 const Banner = ({ blok }) => {
   const { slides } = blok;
   const single = slides.length === 1;
+
+  console.log(blok);
 
   useEffect(() => {
     // eslint-disable-next-line no-unused-vars
@@ -31,59 +34,61 @@ const Banner = ({ blok }) => {
   }, []);
 
   return (
-    <section className="banner-section">
-      <div className="swiper-container banner-slider">
-        <div className="swiper-wrapper">
-          {slides.map((node) => {
-            const { title, copy, cta, link } = node;
-            const innerContent = !!(title || copy || cta || link.url);
+    <SbEditable content={blok}>
+      <section className="banner-section">
+        <div className="swiper-container banner-slider">
+          <div className="swiper-wrapper">
+            {slides.map((node) => {
+              const { title, copy, cta, link } = node;
+              const innerContent = !!(title || copy || cta || link.url);
 
-            return (
-              <div
-                className="swiper-slide"
-                style={{ backgroundImage: `url(${node.image.filename})` }}
-              >
-                <div className="content-outer">
-                  <div className="content-box">
-                    {innerContent && (
-                      <div className="inner">
-                        {title && <h1>{title}</h1>}
-                        {copy && <div className="text">{copy}</div>}
-                        {link.url && (
-                          <div className="link-box">
-                            <a
-                              href={link}
-                              className="theme-btn btn-style-one light"
-                            >
-                              <span>{cta || `FIND OUT MORE`}</span>
-                            </a>
-                          </div>
-                        )}
-                      </div>
-                    )}
+              return (
+                <div
+                  className="swiper-slide"
+                  style={{ backgroundImage: `url(${node.image.filename})` }}
+                >
+                  <div className="content-outer">
+                    <div className="content-box">
+                      {innerContent && (
+                        <div className="inner">
+                          {title && <h1>{title}</h1>}
+                          {copy && <div className="text">{copy}</div>}
+                          {link.url && (
+                            <div className="link-box">
+                              <a
+                                href={link}
+                                className="theme-btn btn-style-one light"
+                              >
+                                <span>{cta || `FIND OUT MORE`}</span>
+                              </a>
+                            </div>
+                          )}
+                        </div>
+                      )}
+                    </div>
                   </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
+          </div>
         </div>
-      </div>
 
-      {!single && (
-        <div className="banner-slider-nav">
-          <div className="banner-slider-control banner-slider-button-prev">
-            <span>
-              <i className="far fa-angle-left" />
-            </span>
+        {!single && (
+          <div className="banner-slider-nav">
+            <div className="banner-slider-control banner-slider-button-prev">
+              <span>
+                <i className="far fa-angle-left" />
+              </span>
+            </div>
+            <div className="banner-slider-control banner-slider-button-next">
+              <span>
+                <i className="far fa-angle-right" />
+              </span>
+            </div>
           </div>
-          <div className="banner-slider-control banner-slider-button-next">
-            <span>
-              <i className="far fa-angle-right" />
-            </span>
-          </div>
-        </div>
-      )}
-    </section>
+        )}
+      </section>
+    </SbEditable>
   );
 };
 
