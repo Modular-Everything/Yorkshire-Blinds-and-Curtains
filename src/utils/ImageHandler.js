ac2b2a84f0b5e36c65f2c6bf001426d3788387cb
Chrish Dunne <hey@chrish.design>
2020-07-17 10:00:10 +0100

    [+] Added image handler

---
+++ src/utils/ImageHandler.js
@@ -0,0 +1,17 @@
+// ====================================
+// ImageHandler for Storyblok
+// ================
+
+module.exports = function ImageHandler(src, option) {
+  const imageService = '//img2.storyblok.com/';
+  const path = src.replace('https://a.storyblok.com', '');
+
+  let size;
+  if (!option) {
+    size = `1920x1080`;
+  } else {
+    size = option;
+  }
+
+  return imageService + size + path;
+};
