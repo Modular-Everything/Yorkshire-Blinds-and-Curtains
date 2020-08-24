0de1bf61167568a9b6b4f58bfa4866d0e82157e6
Chrish Dunne <hey@chrish.design>
2020-08-24 16:34:32 +0100

    [~] Update blocks

--- src/components/blocks/ContactUs/ContactUs.js
+++ src/components/blocks/ContactUs/ContactUs.js
@@ -2,31 +2,37 @@
 import React from 'react';
 import marked from 'marked';
 import parse from 'html-react-parser';
+import SbEditable from 'storyblok-react';
+
 // =====================
 // Contact Us
 // =====================
+
 const ContactUs = ({ blok }) => {
   const { contact_us_info } = blok;
-  console.log(blok);
 
   return (
     <section className="contact-section style-two">
       <div className="auto-container">
         <div className="contact-info-three">
-          <div className="row">
+          <div className="row" style={{ justifyContent: 'center' }}>
             {contact_us_info.map((value, index) => {
+              console.log(value);
+
               return (
-                <div className="col-lg-4" key={index}>
-                  <div className="content">
-                    <div className="icon">
-                      <span className={value.contact_us_icon} />
-                    </div>
-                    <h4>{value.contact_us_title}</h4>
-                    <div className="text">
-                      {parse(marked(value.contact_us_content))}
+                <SbEditable content={value}>
+                  <div className="col-lg-4" key={index}>
+                    <div className="content">
+                      <div className="icon">
+                        <span className={`flaticon-${value.contact_us_icon}`} />
+                      </div>
+                      <h4>{value.contact_us_title}</h4>
+                      <div className="text">
+                        {parse(marked(value.contact_us_content))}
+                      </div>
                     </div>
                   </div>
-                </div>
+                </SbEditable>
               );
             })}
           </div>
