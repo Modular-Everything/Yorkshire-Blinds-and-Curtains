1354a9988a951c440143a133c2fa4aa110f4d37c
Chrish Dunne <hey@chrish.design>
2020-08-25 11:30:36 +0100

    [~] Fixed unclosed comment bug

---
+++ gatsby-browser.js
@@ -0,0 +1,8 @@
+// CSS
+require('bootstrap/dist/css/bootstrap.css');
+require('./static/styles/style.css');
+require('./static/styles/responsive.css');
+require('./static/styles/color.css');
+
+// JS
+require('bootstrap/dist/js/bootstrap.bundle.js');
