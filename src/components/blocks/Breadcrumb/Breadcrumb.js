1e8ab2a204b6a018376754e46e53eaa6b65a63b4
Chrish Dunne <hey@chrish.design>
2020-09-04 16:24:27 +0100

    [~] Full-width title

--- src/components/blocks/Breadcrumb/Breadcrumb.js
+++ src/components/blocks/Breadcrumb/Breadcrumb.js
@@ -17,23 +17,21 @@ const Breadcrumb = ({ blok }) => {
           })`,
         }}
       >
-        <div className="auto-container">
-          <div className="content-box">
-            <div className="content-wrapper">
-              <div className="title">
-                <h1>{breadcrumb_title && breadcrumb_title}</h1>
-              </div>
-              {breadcrumb_subtitle && (
-                <ul className="bread-crumb">
-                  <li className="home">
-                    <a href="index.html">
-                      <span className="far fa-home" />
-                    </a>
-                  </li>
-                  <li>{breadcrumb_subtitle}</li>
-                </ul>
-              )}
+        <div className="content-box">
+          <div className="content-wrapper">
+            <div className="title">
+              <h1>{breadcrumb_title && breadcrumb_title}</h1>
             </div>
+            {breadcrumb_subtitle && (
+              <ul className="bread-crumb">
+                <li className="home">
+                  <a href="index.html">
+                    <span className="far fa-home" />
+                  </a>
+                </li>
+                <li>{breadcrumb_subtitle}</li>
+              </ul>
+            )}
           </div>
         </div>
       </section>
